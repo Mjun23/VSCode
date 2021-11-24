75aab07f1fc800a8a42617fe4aa8404f7e64347b
Mjun23 <ki2017911@gmail.com>
2021-11-24 22:55:40 +0900

    New

---
+++ Code Study/Literal.py
@@ -0,0 +1,14 @@
+a = "I eat %d apples." %3
+print(a)
+
+b = "I eat %s apples." % "five"
+print(b)
+
+number = 3
+c = "I eat %d apples." % number
+print(c)
+
+number2 = 10
+day = "three"
+d = "I ate %d apples. so I was sick for %s days." % (number2, day)
+print(d)
